accbbc2e064f957482c57e342b6e8690f053690e
duanyph <duanyph@qq.com>
2017-11-13 17:54:07 +0800

    小段

---
+++ 学习测试.py
@@ -0,0 +1,36 @@
+from tkinter import *
+import threading,time
+ZhuangTai=0
+GuiJi=[0]*400
+cly=0
+Suo=threading.RLock()
+# tk.geometry("800x800")
+def CaiYang():
+    global ZhuangTai
+    global GuiJi
+    global cly
+    while 1:
+        GuiJi.pop()
+        GuiJi.insert(0,ZhuangTai)
+        cly=1+cly
+def HuiZhi():
+    global GuiJi
+    tk1=Tk()
+    X=400
+    Canvas1=Canvas(width=400,height=400)
+    Canvas1.pack()
+    Canvas1.delete(ALL)
+    while 1:
+        Suo.acquire()
+        for Hua in GuiJi:
+            Canvas1.create_oval(X,Hua,X,Hua)
+            X=X-1
+        tk1.update()
+        X=400
+        Suo.release()
+threading.Thread(target=CaiYang,args=()).start()
+threading.Thread(target=HuiZhi,args=()).start()
+time.sleep(1)
+print(cly)
+while 1:
+    ZhuangTai=input("X:")
