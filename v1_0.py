1ba64db9b7c4b3384d1f2f8a44e5a5b9ce938f77
duanyph <duanyph@qq.com>
2017-11-29 00:22:07 +0800

    小段

--- v1_0.py
+++ v1_0.py
@@ -1,5 +1,7 @@
 #coding:utf-8
 '''
+Created on 2017.11.15
+
 @author: duany
 '''
 from tkinter import *
@@ -7,35 +9,31 @@ import _thread,time
 from RPi import GPIO 
 ZhuangTai=0
 GuiJi=[0]*300
-cyl=0
+#~ cyl=0
+X=1
 tk1=Tk()
 tk1.resizable(0, 0)
+Canvas1=Canvas(width=300,height=200)
+Canvas1.pack()
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
-X=1
-Canvas1=Canvas(width=300,height=200)
-GuiJi=[0]*500
-# cly=0
-tk1=Tk()
-tk1.resizable(0,0)
-X=1
-Canvas1=Canvas(width=500,height=200)
-Canvas1.pack()
-# tk.geometry("800x800")
-"""def HuiZhi():
-    global cyl
-    time.sleep(2)
-    print(cyl)"""
-#~ _thread.start_new_thread(HuiZhi, ())
+# tk1.geometry("800x800")
+def CaiYang():
+    #~ global cyl
+    global GuiJi
+    while 1:
+        GuiJi.pop()
+        GuiJi.insert(0,GPIO.input(7))
+        time.sleep(0.000001)
+        #~ cyl=1+cyl
+_thread.start_new_thread(CaiYang, ())
+#~ time.sleep(1)
+#~ print(cyl)
 while 1:
-    GuiJi.pop()
-    GuiJi.insert(0,GPIO.input(7))
     Canvas1.delete(ALL)
     for Hua in GuiJi:
         Hua=(0-Hua)*100+150
-        Hua=0-Hua+200
         Canvas1.create_rectangle(X,Hua,X,Hua)
         X=X+1
     tk1.update()
     X=1
-    #~ cyl=1+cyl
